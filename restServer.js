44cee298721ec38af8c45c9e35274bb1a5fcf048
KimJiMin <minmin4736@naver.com>
2021-10-24 22:51:31 +0900

    ⚡ :: restServer

--- restServer.js
+++ restServer.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs').promises;
 
+const users = {}; //데이터 저장용
+
 http
 	.createServer(async (req, res) => {
 		try {
@@ -14,13 +16,52 @@ http
 					const data = await fs.readFile('./about.html');
 					res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 					return res.end(data);
+				} else if (req.url === '/users') {
+					res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+					return res.end(JSON.stringify(users));
 				}
-				//주소가 / or /about도 아니면
+				//주소가 / or /about or /users도 아니면
 				try {
 					const data = await fs.readFile(`.${req.url}`);
 					return res.end(data);
 				} catch (err) {
 					//주소에 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
+				}
+			} else if (req.method === 'POST') {
+				if (req.url === '/user') {
+					let body = '';
+					//요청의 body를 stream 형식으로 받음
+					req.on('data', (data) => {
+						body += data;
+					});
+					//요청의 body를 다 받은 후 실행됨
+					return req.on('end', () => {
+						console.log('POST 본문(Body):', body);
+						const { name } = JSON.parse(body);
+						const id = Data.now();
+						users[id] = name;
+						res.writeHead(201);
+						res.end('등록 성공');
+					});
+				}
+			} else if (req.method === 'PUT') {
+				if (req.url.startsWith('/user/')) {
+					const key = req.url.split('/')[2];
+					let body = '';
+					req.on('data', (data) => {
+						body += data;
+					});
+					return req.on('end', () => {
+						console.log('PUT 본문(Body):', body);
+						users[key] = JSON.parse(body).name;
+						return res.end(JSON.stringify(users));
+					});
+				}
+			} else if (req.method === 'DELETE') {
+				if (req.url.startsWith('/users/')) {
+					const key = req.url.split('/')[2];
+					delete users[key];
+					return res.end(JSON.stringify(users));
 				}
 			}
 			res.writeHead(404);
@@ -34,3 +75,11 @@ http
 	.listen(8082, () => {
 		console.log('8082번 포트에서 서버 대기 중');
 	});
+
+//req.method로 HTTP 요청 메서드 구분
+
+/*
+res.end를 호출하면 함수가 종료되지 않음.
+다음 코드가 이어지는 경우에는 return을 사용해 명시적으로 함수를 종료해야함
+return을 붙이지 않고 res.end같은 메서드를 여러 번 실행하게 된다면 에러 발생
+*/
