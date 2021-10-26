584b47bc3158eee0c5ae10aa1ddfb1b87b7ec22b
KimJiMin <minmin4736@naver.com>
2021-10-26 22:35:58 +0900

    ⚡ :: cookie

--- cookie.js
+++ cookie.js
@@ -5,7 +5,7 @@ http
 		console.log(req.url, req.headers.cookie);
 		res.writeHead(200, { 'Set-Cookie': 'mycookie=test' });
 		res.end('Hello Cookie');
-	})
+	}) //서버에서 직접 쿠키를 만들어 요청자의 브라우저에 넣는 작업
 	.listen(8083, () => {
 		console.log('8083번 포트에서 서버 대기 중');
 	});
